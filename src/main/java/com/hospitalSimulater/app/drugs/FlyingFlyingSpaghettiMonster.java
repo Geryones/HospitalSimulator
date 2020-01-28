91b07c2c6c75494bed0691b4f2148c3f8ed7300c
Jurij Maïkoff <sergejurij.maikoff@students.fhnw.ch>
2020-01-28 21:50:36 +0100

    noodly output

--- src/main/java/com/hospitalSimulater/app/drugs/FlyingFlyingSpaghettiMonster.java
+++ src/main/java/com/hospitalSimulater/app/drugs/FlyingFlyingSpaghettiMonster.java
@@ -10,6 +10,8 @@ public class FlyingFlyingSpaghettiMonster extends AbstractDrug implements Drug {
     public State treat(State state) {
         int randomNum = 1 + (int)(Math.random() * ((1000000 - 1) +1));
         if (state.equals(State.Dead) && randomNum == 666) {
+            System.out.println("He who boiled for our sins shows mercy! " +
+                    "With his noodly powers a patient has been resurrected");
             return State.Healthy;
         }
         return state;
